ed8ee3923fe17ac6a6fe8eee7c5d84582ae5c681
Balthild <ibalthild@gmail.com>
2026-04-01 05:54:52 -0500

    feat: show more info in summon stats

---
+++ src/lib/components/rarity.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	interface Props {
+		rarity: 4 | 5 | 6;
+		class?: string;
+	}
+
+	const props: Props = $props();
+</script>
+
+<span class={[`tracking-wider rarity-${props.rarity}`, props.class]}>
+	{props.rarity}✦
+</span>
+
+<style lang="postcss">
+	@reference '$lib/styles/index.css';
+
+	@layer components {
+		.rarity-6 {
+			@apply text-amber-600;
+		}
+
+		.rarity-5 {
+			color: color-mix(in oklch, var(--color-yellow-500), var(--color-black) 5%);
+		}
+
+		.rarity-4 {
+			@apply text-violet-500;
+		}
+	}
+</style>
--- src/routes/summon/+page.svelte
+++ src/routes/summon/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { liveQuery } from 'dexie';
-	import { SvelteMap, SvelteSet } from 'svelte/reactivity';
+	import { untrack } from 'svelte';
+	import { SvelteMap } from 'svelte/reactivity';
 	import * as v from 'valibot';
 
-	import { idb } from '$lib/idb';
+	import Rarity from '$lib/components/rarity.svelte';
+	import { idb, type Summon } from '$lib/idb';
 	import { validate } from '$lib/template/validate.svelte';
 	import { distinct } from '$lib/utils';
 
@@ -22,17 +24,25 @@
 		alert(`导入完成，新增 ${count} 条记录`);
 	};
 
-	const summons = liveQuery(() => idb.summons.orderBy('record.createTime').reverse().toArray());
+	const rawSummons = liveQuery(() => idb.summons.orderBy('record.createTime').reverse().toArray());
 
-	const userIds = $derived(distinct($summons?.map((it) => it.userId)).sort());
+	const userIds = $derived(distinct($rawSummons?.map((it) => it.userId)).sort());
+
+	const userSummons = $derived.by(() => {
+		const summons = new SvelteMap(userIds.map((id) => [id, [] as Summon[]]));
+		for (const summon of $rawSummons ?? []) {
+			summons.get(summon.userId)!.push(summon);
+		}
+
+		return summons;
+	});
 
 	// TODO: sort in predefined order
-	const poolTypes = $derived(distinct($summons?.map((it) => it.record.poolType)));
+	const poolTypes = $derived(distinct($rawSummons?.map((it) => it.record.poolType)));
 
 	const poolNames = $derived.by(() => {
 		const names = new SvelteMap<number, string>();
-
-		for (const summon of $summons ?? []) {
+		for (const summon of $rawSummons ?? []) {
 			if (!names.has(summon.record.poolType)) {
 				names.set(summon.record.poolType, summon.record.poolName);
 			}
@@ -41,63 +51,95 @@
 		return names;
 	});
 
-	const poolPities = $derived.by(() => {
-		const pities = new SvelteMap<number, number>();
-		const done = new SvelteSet<number>();
-
-		for (const summon of $summons ?? []) {
-			if (done.has(summon.record.poolType)) {
-				continue;
-			}
-
-			let pity = pities.get(summon.record.poolType) ?? 0;
-
-			for (const gainId of summon.record.gainIds.reverse()) {
-				const arcanistId = String(gainId);
-				if (data.arcanists[arcanistId].rarity < 6) {
-					pity++;
-					continue;
-				} else {
-					done.add(summon.record.poolType);
-					break;
-				}
-			}
-
-			pities.set(summon.record.poolType, pity);
-		}
-
-		return pities;
-	});
-
 	let selectedUserId = $state('');
 	let selectedPoolType = $state(0);
 
 	// looks like an anti-pattern but it works as for now ¯\_(ツ)_/¯
 	$effect(() => {
-		if (!selectedUserId && userIds.length > 0) {
+		if (!userIds.includes(untrack(() => selectedUserId))) {
 			selectedUserId = userIds[0];
 		}
 
-		if (!selectedPoolType && poolTypes.length > 0) {
+		if (!poolTypes.includes(untrack(() => selectedPoolType))) {
 			selectedPoolType = poolTypes[0];
 		}
 	});
 
-	const presenting = $derived.by(() => {
-		return Iterator.from($summons ?? [])
-			.filter((it) => it.userId === selectedUserId)
-			.filter((it) => it.record.poolType === selectedPoolType)
-			.flatMap((it) =>
-				it.record.gainIds.reverse().map((gainId) => {
+	interface Gain {
+		id: number;
+		time: string;
+		name: string;
+		rarity: number;
+	}
+
+	interface PastGain extends Gain {
+		invested: number;
+	}
+
+	const poolGains = $derived.by(() => {
+		const gains = new SvelteMap(poolTypes.map((type) => [type, [] as Gain[]]));
+
+		for (const summon of userSummons.get(selectedUserId) ?? []) {
+			gains.get(summon.record.poolType)!.push(
+				...summon.record.gainIds.toReversed().map((gainId) => {
 					const arcanistId = String(gainId);
 					return {
-						time: it.record.createTime,
+						id: gainId,
+						time: summon.record.createTime,
 						name: data.arcanists[arcanistId].name,
 						rarity: data.arcanists[arcanistId].rarity,
 					};
 				}),
-			)
-			.toArray();
+			);
+		}
+
+		return gains;
+	});
+
+	const poolPities6 = $derived.by(() => {
+		const pities = new SvelteMap<number, number>();
+		for (const [pool, gain] of poolGains.entries()) {
+			const pity = gain.findIndex((it) => it.rarity === 6);
+			pities.set(pool, pity === -1 ? gain.length : pity);
+		}
+
+		return pities;
+	});
+
+	const gains = $derived(poolGains.get(selectedPoolType) ?? []);
+
+	const calculatePastGain = (gains: Gain[], rarity: number) => {
+		const indices = [];
+		const past: PastGain[] = [];
+
+		for (const [index, gain] of gains.toReversed().entries()) {
+			if (gain.rarity === rarity) {
+				const last = indices[indices.length - 1] ?? 0;
+				past.push({ ...gain, invested: index - last });
+				indices.push(index);
+			}
+		}
+
+		return past.reverse();
+	};
+
+	let selectedRarity: 6 | 5 = $state(6);
+	const pastGains6 = $derived(calculatePastGain(gains, 6));
+	const pastGains5 = $derived(calculatePastGain(gains, 5));
+	const pastGains = $derived({ 6: pastGains6, 5: pastGains5 }[selectedRarity]);
+
+	const count6 = $derived(gains.filter((it) => it.rarity === 6).length ?? 0);
+	const count5 = $derived(gains.filter((it) => it.rarity === 5).length ?? 0);
+	const count4 = $derived(gains.filter((it) => it.rarity === 4).length ?? 0);
+
+	const average6 = $derived.by(() => {
+		const total = pastGains6.reduce((sum, it) => sum + it.invested, 0);
+		return total / count6;
+	});
+
+	const average5 = $derived.by(() => {
+		const total = pastGains5.reduce((sum, it) => sum + it.invested, 0);
+		return total / count5;
 	});
 </script>
 
@@ -153,8 +195,8 @@
 				class:active={poolType === selectedPoolType}
 				onclick={() => (selectedPoolType = poolType)}
 			>
-				<p class="text-lg font-semibold">{poolPities.get(poolType)}&ThinSpace;/&ThinSpace;70</p>
-				<p class="text-xs font-medium"><span class="rarity-6">6✦</span> 保底</p>
+				<p class="text-lg font-semibold">{poolPities6.get(poolType)}&ThinSpace;/&ThinSpace;70</p>
+				<p class="text-xs font-medium"><Rarity rarity={6} /> 保底</p>
 				<p class="text-sm font-medium mt-2 mb-px">{poolNames.get(poolType)}</p>
 			</button>
 		{/each}
@@ -169,19 +211,19 @@
 			<dl class="space-y-1 text-sm tabular-nums">
 				<div class="flex justify-between">
 					<dt>总计征集次数</dt>
-					<dd class="font-medium">{presenting.length}</dd>
+					<dd class="font-medium">{gains.length}</dd>
 				</div>
 				<div class="flex justify-between">
-					<dt><span class="rarity-6">6✦</span> 获取次数</dt>
-					<dd class="font-medium">{presenting.filter((it) => it.rarity === 6).length}</dd>
+					<dt><Rarity rarity={6} /> 获取次数</dt>
+					<dd class="font-medium">{count6}</dd>
 				</div>
 				<div class="flex justify-between">
-					<dt><span class="rarity-5">5✦</span> 获取次数</dt>
-					<dd class="font-medium">{presenting.filter((it) => it.rarity === 5).length}</dd>
+					<dt><Rarity rarity={5} /> 获取次数</dt>
+					<dd class="font-medium">{count5}</dd>
 				</div>
 				<div class="flex justify-between">
-					<dt><span class="rarity-4">4✦</span> 获取次数</dt>
-					<dd class="font-medium">{presenting.filter((it) => it.rarity === 4).length}</dd>
+					<dt><Rarity rarity={4} /> 获取次数</dt>
+					<dd class="font-medium">{count4}</dd>
 				</div>
 			</dl>
 		</section>
@@ -191,18 +233,53 @@
 
 			<dl class="space-y-1 text-sm tabular-nums">
 				<div class="flex justify-between">
-					<dt><span class="rarity-6">6✦</span> 平均征集次数</dt>
-					<dd class="font-medium">TODO</dd>
+					<dt><Rarity rarity={6} /> 平均征集次数</dt>
+					<dd class="font-medium">{isNaN(average6) ? '无数据' : average6.toFixed(2)}</dd>
 				</div>
 				<div class="flex justify-between">
-					<dt><span class="rarity-5">5✦</span> 平均征集次数</dt>
-					<dd class="font-medium">TODO</dd>
+					<dt><Rarity rarity={5} /> 平均征集次数</dt>
+					<dd class="font-medium">{isNaN(average5) ? '无数据' : average5.toFixed(2)}</dd>
 				</div>
 			</dl>
 		</section>
 
-		<section class="panel col-span-2">
-			<pre class="text-sm">{JSON.stringify(presenting, null, 2)}</pre>
+		<section class="border border-gray-300 bg-white/50 col-span-2">
+			<header class="flex flex-row items-stretch text-ms border-b border-gray-300">
+				<button
+					class="py-1.25 flex-1 cursor-pointer"
+					class:bg-stripe={selectedRarity === 6}
+					class:bg-gray-50={selectedRarity === 6}
+					onclick={() => (selectedRarity = 6)}
+				>
+					<Rarity rarity={6} class="-mr-px" />
+				</button>
+				<div class="border-r border-gray-300"></div>
+				<button
+					class="py-1.25 flex-1 cursor-pointer"
+					class:bg-stripe={selectedRarity === 5}
+					class:bg-gray-50={selectedRarity === 5}
+					onclick={() => (selectedRarity = 5)}
+				>
+					<Rarity rarity={5} class="-mr-px" />
+				</button>
+			</header>
+			<ol class="gains min-h-8">
+				{#each pastGains as gain (`${gain.time},${gain.id}`)}
+					<li class="flex flex-col items-center p-2 pb-4">
+						<div class="aspect-4/7 m-[12%] mb-0 border border-gray-200 bg-gray-50 bg-stripe rounded-t-full overflow-hidden">
+							<img
+								src="https://cdn.jsdelivr.net/gh/myssal/Reverse-1999-CN-Asset/singlebg/headicon_middle/{gain.id}01.png"
+								alt={gain.name}
+								class=""
+							/>
+						</div>
+						<p class="text-ml font-medium text-gray-600 mt-3">{gain.name}</p>
+						<p class="text-sm font-normal">({gain.invested})</p>
+					</li>
+				{:else}
+					<li class="text-gray-500 text-center border-0 col-span-full p-4">无数据</li>
+				{/each}
+			</ol>
 		</section>
 	</main>
 </section>
@@ -210,36 +287,40 @@
 <style lang="postcss">
 	@reference '$lib/styles/index.css';
 
-	.pool {
-		@apply border border-dashed border-gray-400/60 hover:border-gray-400 rounded-xs;
-		@apply px-3 py-2;
-		@apply cursor-pointer;
-		@apply transition-colors;
+	@layer components {
+		.pool {
+			@apply border border-dashed border-gray-400/60 hover:border-gray-400 rounded-xs;
+			@apply px-3 py-2;
+			@apply cursor-pointer;
+			@apply transition-colors;
 
-		&.active {
-			@apply border-solid border-gray-400/80 bg-white/50;
-			@apply cursor-default;
+			&.active {
+				@apply border-solid border-gray-400/80 bg-white/50;
+				@apply cursor-default;
+			}
 		}
-	}
 
-	.panel {
-		@apply px-3.5 py-3;
-		@apply border border-gray-300 bg-white/50;
-		@apply rounded-xs;
-	}
+		.panel {
+			@apply px-3.5 py-3;
+			@apply border border-gray-300 bg-white/50;
+			@apply rounded-xs;
+		}
 
-	.rarity-6 {
-		@apply text-amber-600;
-		@apply tracking-wider;
-	}
+		.gains {
+			@apply grid grid-cols-5;
 
-	.rarity-5 {
-		color: color-mix(in oklch, var(--color-yellow-500), var(--color-black) 5%);
-		@apply tracking-wider;
-	}
+			li:not(.empty) {
+				@apply border-r border-b border-gray-300;
+			}
 
-	.rarity-4 {
-		@apply text-violet-500;
-		@apply tracking-wider;
+			li:nth-child(5n) {
+				@apply border-r-0;
+			}
+
+			li:nth-child(5n+1):nth-last-child(-n+5),
+			li:nth-child(5n+1):nth-last-child(-n+5) ~ li {
+				@apply border-b-0;
+			}
+		}
 	}
 </style>
