895d849d5bd34b0aabed7e5961ecd2095e73a87e
Balthild <ibalthild@gmail.com>
2026-04-04 11:58:40 -0500

    feat: update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,38 +1,47 @@
-<section class="p-6 space-y-2">
-	<h2 class="text-ml font-bold">关于</h2>
-	<ul class="space-y-1.5 list-disc ml-3.75">
+<script lang="ts"></script>
+
+{#snippet external(url: string)}
+	<a href={url} target="_blank" rel="external noopener noreferrer" class="text-link">{url}</a>
+{/snippet}
+
+<section class="p-6 space-y-3">
+	<h2 class="text-xl font-bold">关于</h2>
+
+	<p>
+		本项目是以 GNU Affero General Public License v3.0
+		许可协议发表的自由软件，您可以在此处获取它的源代码副本：
+	</p>
+
+	<ul class="space-y-2 list-disc ml-3.75">
 		<li>
-			GitHub
+			GitHub Repository
 			<br />
-			<a
-				href="https://github.com/balthild/improviser1999"
-				target="_blank"
-				rel="noopener noreferrer"
-				class="text-link"
-			>https://github.com/balthild/improviser1999</a>
+			{@render external('https://github.com/balthild/improviser1999')}
 		</li>
 	</ul>
-	<p>本项目使用了来自以下项目的数据或资源文件：</p>
-	<ul class="space-y-1.5 list-disc ml-3.75">
+
+	<p>本项目还使用或展示了来自以下项目的数据或资源文件，它们的许可协议请参阅各自的项目页面：</p>
+
+	<ul class="space-y-2 list-disc ml-3.75">
 		<li>
 			Reverse1999 Assets
 			<br />
-			<a
-				href="https://github.com/myssal/Reverse-1999-CN-Asset"
-				target="_blank"
-				rel="noopener noreferrer"
-				class="text-link"
-			>https://github.com/myssal/Reverse-1999-CN-Asset</a>
+			{@render external('https://github.com/myssal/Reverse-1999-CN-Asset')}
 		</li>
 		<li>
 			必要的记录 by 伴春风而归
 			<br />
-			<a
-				href="https://www.kdocs.cn/l/cd5MWeCl5bKw"
-				target="_blank"
-				rel="noopener noreferrer"
-				class="text-link"
-			>https://www.kdocs.cn/l/cd5MWeCl5bKw</a>
+			{@render external('https://www.kdocs.cn/l/cd5MWeCl5bKw')}
+		</li>
+		<li>
+			重返未来 1999 中文维基
+			<br />
+			{@render external('https://res1999.huijiwiki.com')}
+		</li>
+		<li>
+			Reverse: 1999 Wiki
+			<br />
+			{@render external('https://reverse1999.fandom.com')}
 		</li>
 	</ul>
 </section>
