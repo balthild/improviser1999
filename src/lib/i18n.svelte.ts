65a622010b25245f3e003d368489cc76959aaadc
Balthild <ibalthild@gmail.com>
2026-04-10 08:07:49 -0500

    feat: remember language setting

--- src/lib/i18n.svelte.ts
+++ src/lib/i18n.svelte.ts
@@ -23,30 +23,48 @@ export function getLanguage(): Language {
 
 export function setLanguage(language: Language) {
 	current = language;
+	document.cookie = `improviser1999_language=${language}; max-age=34560000; path=/`;
 }
 
 function getDefaultLanguage(): Language {
-	return browser ? getBrowserDefaultLanguage() : getServerDefaultLanguage();
+	if (browser) {
+		return resolveRenderedLanguage() ?? resolveNavigatorLanguage() ?? 'zh';
+	} else {
+		return resolveCookiesLanguage() ?? resolveAcceptLanguage() ?? 'zh';
+	}
 }
 
-function getBrowserDefaultLanguage(): Language {
+function resolveRenderedLanguage(): Language | undefined {
+	const language = document.documentElement.lang;
+	if (Object.hasOwn(languages, language)) {
+		return language as Language;
+	}
+}
+
+function resolveNavigatorLanguage(): Language | undefined {
 	for (const language of navigator.languages) {
 		const short = language.substring(0, 2);
 		if (Object.hasOwn(languages, short)) {
 			return short as Language;
 		}
 	}
-
-	return 'zh';
 }
 
-function getServerDefaultLanguage(): Language {
+function resolveCookiesLanguage(): Language | undefined {
+	const { cookies } = getRequestEvent();
+
+	const language = cookies.get('improviser1999_language');
+	if (language && Object.hasOwn(languages, language)) {
+		return language as Language;
+	}
+}
+
+function resolveAcceptLanguage(): Language | undefined {
 	const { request } = getRequestEvent();
 
-	// TODO: get from cookies first
 	const headers = { 'accept-language': request.headers.get('accept-language') ?? '' };
 	const negotiator = new Negotiator({ headers });
-	const language = negotiator.language(Object.keys(languages)) ?? 'zh';
+	const language = negotiator.language(Object.keys(languages));
 
-	return language as Language;
+	return language as Language | undefined;
 }
